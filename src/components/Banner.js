55cbe9d4638b423bfe957178116f1b1d66e0b2be
Icaroesr <100791677+icaroedu@users.noreply.github.com>
2025-07-14 07:05:46 -0700

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -56,9 +56,10 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Ícaro`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Dev Front-End" ]'><span className="wrap">{text}</span></span></h1>
-                  <p> Olá sou desenvolvedor Front-end apaixonado cada vez mais, gosto de sempre evoluir 
-                    e buscar novas tecnologias.
+                <h1>{`Hi! I'm Ícaro Eduardo`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Dev Front-End" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>  I’m a passionate software developer who loves turning ideas into powerful, user-friendly digital experiences. 
+                      With a strong focus on front-end development, clean code, and scalable solutions, 
+                      I build applications that are both functional and elegant.
                   </p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
